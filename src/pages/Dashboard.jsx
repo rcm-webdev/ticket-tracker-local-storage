ee6b4746d27f5b24eca72e2d76452e245b6edab8
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-04-16 15:02:39 -0700

    separate header to the list of tickets

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,16 @@
+import { useState, useEffect } from "react";
 import BackButton from "../components/BackButton";
 import Form from "../components/Form";
-import SignupForm from "../components/SignupForm";
 import TicketList from "../components/TicketList";
+import { getTickets } from "../utils/storage";
 
 export default function Dashboard() {
+  const [tickets, setTickets] = useState([]);
+
+  useEffect(() => {
+    setTickets(getTickets());
+  }, []);
+
   return (
     <div className="space-y-6">
       <div className="bg-base-300">
@@ -15,14 +22,17 @@ export default function Dashboard() {
         <h1 className="text-4xl font-bold mb-6">Welcome to Dashboard</h1>
         <div className="flex gap-5 justify-between">
           <div className="w-1/2">
-            <Form />
+            <Form setTickets={setTickets} />
           </div>
-          <div className="w-1/2 overflow-y-auto h-[500px]">
-            {/* <li className="block font-bold bg-base-200 p-6 rounded-3xl hover:bg-neutral hover:text-neutral-content duration-200">
+          <div className="w-1/2">
+            <h1 className="font-bold text-2xl">Tickets</h1>
+            <div className="overflow-y-auto h-[500px]">
+              {/* <li className="block font-bold bg-base-200 p-6 rounded-3xl hover:bg-neutral hover:text-neutral-content duration-200">
               Ticket #4
             </li> */}
 
-            <TicketList />
+              <TicketList tickets={tickets} setTickets={setTickets} />
+            </div>
           </div>
         </div>
       </div>
